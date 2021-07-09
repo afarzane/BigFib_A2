e83bc3b8d3f0ad3e7c31ff8bcb8a7337ec791397
afarzane <afarzane@sfu.ca>
2021-07-08 17:24:55 -0700

    a dding all files to repo

---
+++ BigFib.c
@@ -0,0 +1,55 @@
+/******************************************************************************
+;@
+;@ Student Name 1: student1
+;@ Student 1 #: 123456781
+;@ Student 1 userid (email): stu1 (stu1@sfu.ca)
+;@
+;@ Student Name 2: student2
+;@ Student 2 #: 123456782
+;@ Student 2 userid (email): stu2 (stu2@sfu.ca)
+;@
+;@ Below, edit to list any people who helped you with the code in this file,
+;@      or put ‘none’ if nobody helped (the two of) you.
+;@
+;@ Helpers: _everybody helped us/me with the assignment (list names or put ‘none’)__
+;@
+;@ Also, reference resources beyond the course textbooks and the course pages on Canvas
+;@ that you used in making your submission.
+;@
+;@ Resources:  ___________
+;@
+;@% Instructions:
+;@ * Put your name(s), student number(s), userid(s) in the above section.
+;@ * Edit the "Helpers" line and "Resources" line.
+;@ * Your group name should be "<userid1>_<userid2>" (eg. stu1_stu2)
+;@ * Form groups as described at:  https://courses.cs.sfu.ca/docs/students
+;@ * Submit your file to courses.cs.sfu.ca
+;@
+;@ Name        : BigFib.c
+;@ Description : bigFib subroutine for HW5.
+******************************************************************************/
+
+#include <stdlib.h>
+
+typedef unsigned int bigNumN[];
+
+int bigAdd(bigNumN bigN0P, const bigNumN bigN1P, unsigned int maxN0Size);
+
+int bigFib(int n, int maxSize, unsigned **bNP) {
+
+	unsigned* bNa = malloc(4*(1 + maxSize));
+	// check for null pointer being returned.
+	unsigned* bNb = malloc(4*(1 + maxSize));
+	// check for null pointer being returned.
+
+	if (bNa)
+		*bNa = 0;
+	
+	// ... fill in code here ****
+	
+	// The following two lines of code are just examples.
+	// You might not always want to do them.
+	*bNP = bNa; // don't forget to free the other allocated memory
+	return 0;
+}
+
---
+++ Flash/bigfib.d
@@ -0,0 +1,2 @@
+.\flash\bigfib.o: BigFib.c
+.\flash\bigfib.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
